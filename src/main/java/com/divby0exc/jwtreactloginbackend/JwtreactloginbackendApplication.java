f285fe7ba9b5606d2c0cbba548ec5ece35ed0dc3
divby0exc <seddigh.daniel@gmail.com>
2023-05-31 00:37:58 +0200

    what did i do?

---
+++ src/main/java/com/divby0exc/jwtreactloginbackend/JwtreactloginbackendApplication.java
@@ -0,0 +1,13 @@
+package com.divby0exc.jwtreactloginbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtreactloginbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtreactloginbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/divby0exc/jwtreactloginbackend/JwtreactloginbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divby0exc.jwtreactloginbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtreactloginbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
